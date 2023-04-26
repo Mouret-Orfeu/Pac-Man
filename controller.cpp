60281cbf267d11b17f45dea84ff48543cb847b09
Orfeu <orfeu8@live.fr>
2023-04-26 17:57:34 +0200

    factorisation check input

--- controller.cpp
+++ controller.cpp
@@ -8,28 +8,13 @@ controller::direction_t controller::check_input()
     const Uint8* keys = SDL_GetKeyboardState(&nbk);
 
     if (keys[SDL_SCANCODE_LEFT])
-    {
-        puts("LEFT");
         direction= direction_t::left;
-    }
-
     if (keys[SDL_SCANCODE_RIGHT])
-    {
-        puts("RIGHT");
         direction= direction_t::right;
-    }
-
     if (keys[SDL_SCANCODE_UP])
-    {
-        puts("UP");
         direction= direction_t::up;
-    }
-
     if (keys[SDL_SCANCODE_DOWN])
-    {
-        puts("DOWN");
         direction= direction_t::down;
-    }
 
     return direction;
 }
