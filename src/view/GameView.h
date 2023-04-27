a3bb0d284e2d215900f0c484080ce1a777ae0859
Maxime Drouhin <maxime.drouhin@etu.unistra.fr>
2023-04-27 19:06:12 +0200

    Cleaned `GameView.h`

--- src/view/GameView.h
+++ src/view/GameView.h
@@ -34,13 +34,16 @@ private:
     SDL_Surface* plancheSprites_prof = nullptr;
     SDL_Surface* plancheSprites_originale = nullptr;
 
-    // Constants for the tile size and window/maze dimensions
-    static constexpr int TILE_SIZE = 24;
+    // Constants for the tile size and upscaling factor
+    static constexpr int UPSCALING_FACTOR = 3;
     static constexpr int ORIGINAL_TILE_SIZE = 8;
-    static constexpr int MAP_COLS = 28;
-    static constexpr int MAP_ROWS = 36;
-    static constexpr int MAZE_COLS = 28;
-    static constexpr int MAZE_ROWS = 31;
+    static constexpr int TILE_SIZE = UPSCALING_FACTOR*ORIGINAL_TILE_SIZE;
+
+    // Constants for the window/maze dimensions
+    static constexpr int MAP_COLS = 28,
+                         MAP_ROWS = 36;
+    static constexpr int MAZE_COLS = 28,
+                         MAZE_ROWS = 31;
     static constexpr int WINDOW_WIDTH  = MAP_COLS*TILE_SIZE,
                          WINDOW_HEIGHT = MAP_ROWS*TILE_SIZE;
     static constexpr int MAZE_WIDTH  = MAZE_COLS*TILE_SIZE,
@@ -48,68 +51,23 @@ private:
 
     // Constants for the top left corner of the maze and various sprites
     static constexpr SDL_Point map_top_left = {0,48};
-    static constexpr SDL_Rect map_sprite_originale = {
-        0,0,
-        MAZE_COLS*ORIGINAL_TILE_SIZE,MAZE_ROWS*ORIGINAL_TILE_SIZE
-    }; // x,y, w,h (0,0) en haut a gauche
-    static constexpr SDL_Rect map_sprite_prof = {
-        200,3,
-        168,216
-    };
-    static constexpr SDL_Rect zero_sprite = {
-        3,52,
-        ORIGINAL_TILE_SIZE,ORIGINAL_TILE_SIZE
-    };
-    static constexpr SDL_Rect one_sprite = {
-        11,52,
-        ORIGINAL_TILE_SIZE,ORIGINAL_TILE_SIZE
-    };
-    static constexpr SDL_Rect U_sprite = {
-        43,68,
-        ORIGINAL_TILE_SIZE,ORIGINAL_TILE_SIZE
-    };
-    static constexpr SDL_Rect P_sprite = {
-        3,68,
-        ORIGINAL_TILE_SIZE,ORIGINAL_TILE_SIZE
-    };
+    static constexpr SDL_Rect map_sprite_originale = { 0,0, MAZE_COLS*ORIGINAL_TILE_SIZE,MAZE_ROWS*ORIGINAL_TILE_SIZE }; // x,y, w,h (0,0) en haut a gauche
+    static constexpr SDL_Rect map_sprite_prof = { 200,3, 168,216 };
+    static constexpr SDL_Rect zero_sprite = { 3,52, ORIGINAL_TILE_SIZE,ORIGINAL_TILE_SIZE };
+    static constexpr SDL_Rect one_sprite = { 11,52, ORIGINAL_TILE_SIZE,ORIGINAL_TILE_SIZE };
+    static constexpr SDL_Rect U_sprite = { 43,68, ORIGINAL_TILE_SIZE,ORIGINAL_TILE_SIZE };
+    static constexpr SDL_Rect P_sprite = { 3,68, ORIGINAL_TILE_SIZE,ORIGINAL_TILE_SIZE };
 
     // SDL rectangles for maze and HUP elements
-    SDL_Rect bg = {
-        0,3*TILE_SIZE,
-        3*MAP_COLS*ORIGINAL_TILE_SIZE,3*MAZE_ROWS*ORIGINAL_TILE_SIZE
-    };
-    SDL_Rect first_score_number_pos = {
-        5*TILE_SIZE,TILE_SIZE,
-        TILE_SIZE-2,TILE_SIZE-2
-    };
-    SDL_Rect second_score_number_pos = {
-        6*TILE_SIZE,TILE_SIZE,
-        TILE_SIZE-2,TILE_SIZE-2
-    };
-    SDL_Rect one_up_one_pos = {
-        3*TILE_SIZE,0,
-        TILE_SIZE-2,TILE_SIZE-2
-    };
-    SDL_Rect one_up_U_pos = {
-        4*TILE_SIZE,0,
-        TILE_SIZE-2,TILE_SIZE-2
-    };
-    SDL_Rect one_up_P_pos = {
-        5*TILE_SIZE,0,
-        TILE_SIZE-2,TILE_SIZE-2
-    };
-    SDL_Rect first_life_pos = {
-        2*TILE_SIZE,WINDOW_HEIGHT-2*TILE_SIZE,
-        2*TILE_SIZE-5,2*TILE_SIZE-5
-    };
-    SDL_Rect second_life_pos = {
-        4*TILE_SIZE,WINDOW_HEIGHT-2*TILE_SIZE,
-        2*TILE_SIZE-5,2*TILE_SIZE-5
-    };
-    SDL_Rect third_life_pos = {
-        6*TILE_SIZE,WINDOW_HEIGHT-2*TILE_SIZE,
-        2*TILE_SIZE-5,2*TILE_SIZE-5
-    };
+    SDL_Rect bg = { 0,3*TILE_SIZE, MAP_COLS*TILE_SIZE,MAZE_ROWS*TILE_SIZE };
+    SDL_Rect first_score_number_pos = { 5*TILE_SIZE,TILE_SIZE, TILE_SIZE-2,TILE_SIZE-2 };
+    SDL_Rect second_score_number_pos = { 6*TILE_SIZE,TILE_SIZE, TILE_SIZE-2,TILE_SIZE-2 };
+    SDL_Rect one_up_one_pos = { 3*TILE_SIZE,0, TILE_SIZE-2,TILE_SIZE-2 };
+    SDL_Rect one_up_U_pos = { 4*TILE_SIZE,0, TILE_SIZE-2,TILE_SIZE-2 };
+    SDL_Rect one_up_P_pos = { 5*TILE_SIZE,0, TILE_SIZE-2,TILE_SIZE-2 };
+    SDL_Rect first_life_pos = { 2*TILE_SIZE,WINDOW_HEIGHT-2*TILE_SIZE, 2*TILE_SIZE-5,2*TILE_SIZE-5 };
+    SDL_Rect second_life_pos = { 4*TILE_SIZE,WINDOW_HEIGHT-2*TILE_SIZE, 2*TILE_SIZE-5,2*TILE_SIZE-5 };
+    SDL_Rect third_life_pos = { 6*TILE_SIZE,WINDOW_HEIGHT-2*TILE_SIZE, 2*TILE_SIZE-5,2*TILE_SIZE-5 };
 
     // Constants for the map dimensions and layout
     static constexpr int MAP[MAP_ROWS][MAP_COLS] = {
